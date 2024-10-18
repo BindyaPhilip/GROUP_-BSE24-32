2ff930f661104e9e057778d807b070dadef81ea7
Bindya Philip <philbindya55@gmail.com>
2024-10-18 10:45:25 +0300

    made changes in the settings

--- cakeaddicts/settings.py
+++ cakeaddicts/settings.py
@@ -189,6 +189,8 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage' 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 #STATICFILES_DIRS = [BASE_DIR / "static"]
